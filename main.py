a996752f4c8a90508e6662ed4c26ebe6f7583817
MariKirikova <kirikovamasha@gmail.com>
2022-10-19 15:40:08 +0300

    new program code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Lucky ticket!')
+def calculate_sum(number):
+    d_sum = 0
+    while number > 0:
+        d_sum = d_sum + number%10
+        number = number // 10
+    return d_sum
+ 
+def check_ticket(a, b):
+    if a == b:
+        return True
+    else:
+        return False
+ 
+n = int(input("Enter the ticket number:"))
+if check_ticket(calculate_sum(n//1000),calculate_sum(n%1000)):
+    print('Lucky ticket!')
